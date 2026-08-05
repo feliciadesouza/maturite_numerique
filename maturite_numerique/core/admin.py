9eba9144b8fbf309a1cb12975688f38dce8ee3ba
feliciadesouza <feliciaodetted@gmail.com>
2026-08-05 09:05:55 +0000

    Improve Django admin usability

--- maturite_numerique/core/admin.py
+++ maturite_numerique/core/admin.py
@@ -14,12 +14,15 @@ class OptionReponseInline(admin.TabularInline):
 class DimensionAdmin(admin.ModelAdmin):
     list_display = ("nom", "poids", "ordre", "actif")
     list_editable = ("poids", "ordre", "actif")
+    list_filter = ("actif",)
+    search_fields = ("nom", "description")
     ordering = ("ordre",)
 
 
 @admin.register(TypeChamp)
 class TypeChampAdmin(admin.ModelAdmin):
     list_display = ("libelle", "code")
+    search_fields = ("libelle", "code")
 
 
 @admin.register(Formulaire)
@@ -31,6 +34,7 @@ class FormulaireAdmin(admin.ModelAdmin):
 class VersionFormulaireAdmin(admin.ModelAdmin):
     list_display = ("formulaire", "numero_version", "date_creation", "est_active")
     list_filter = ("formulaire", "est_active")
+    search_fields = ("formulaire__code", "formulaire__nom", "numero_version")
 
 
 @admin.register(Question)
@@ -39,7 +43,9 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ("dimension", "version_formulaire", "type_champ", "actif")
     list_editable = ("ordre", "actif")
     search_fields = ("code", "texte")
+    autocomplete_fields = ("dimension", "version_formulaire", "type_champ", "question_condition")
     inlines = [OptionReponseInline]
+    list_per_page = 25
 
     def texte_court(self, obj):
         return obj.texte[:70]
@@ -49,23 +55,31 @@ class QuestionAdmin(admin.ModelAdmin):
 @admin.register(Administration)
 class AdministrationAdmin(admin.ModelAdmin):
     list_display = ("nom", "secteur", "region", "pays")
-    search_fields = ("nom",)
+    list_filter = ("pays", "region", "secteur")
+    search_fields = ("nom", "secteur", "region", "pays")
+    ordering = ("nom",)
 
 
 @admin.register(Utilisateur)
 class UtilisateurAdmin(admin.ModelAdmin):
     list_display = ("user", "role", "administration")
-    list_filter = ("role",)
+    list_filter = ("role", "administration")
+    search_fields = ("user__username", "user__first_name", "user__last_name", "administration__nom")
+    autocomplete_fields = ("user", "administration")
 
 
 @admin.register(Agent)
 class AgentAdmin(admin.ModelAdmin):
     list_display = ("poste", "administration", "tranche_age", "mode_saisie", "niveau_maturite")
     list_filter = ("administration", "mode_saisie", "tranche_age")
+    search_fields = ("poste", "service", "administration__nom")
+    autocomplete_fields = ("administration",)
 
 
 @admin.register(Reponse)
 class ReponseAdmin(admin.ModelAdmin):
     list_display = ("question", "valeur", "administration", "agent", "date_reponse")
-    list_filter = ("question__dimension",)
+    list_filter = ("question__dimension", "administration", "agent")
+    search_fields = ("question__code", "question__texte", "valeur", "administration__nom", "agent__poste")
+    autocomplete_fields = ("question", "administration", "agent", "utilisateur")
     date_hierarchy = "date_reponse"
